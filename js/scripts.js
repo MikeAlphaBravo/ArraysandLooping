8341bb85b95dd254ac9ac338258b94feb602d1b9
Michael Brooks <silverfox@Michaels-MacBook-Pro.local>
2017-06-22 16:26:19 -0700

    Establish all base HTML and business logic untested.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-//business logic
-for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
+//business logic (Back-end)
 
+var conditional = function (userInput) {
+  for (var currentNumber = 1; currentNumber <= userInput; currentNumber 1++) {
+    if (currentNumber % 15 === 0) {
+      $("#results").append("<li>PING PONG!</li>");
+    } else if (currentNumber % 5 === 0) {
+      $("#results").append("<li>pong</li>");
+    } else if (currentNumber % 3 === 0) {
+      $("#results").append("<li>ping</li>");
+    } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
+      $("#results").append("<li>" + currentNumber + "</li>");
+    }
+  };
+};
 
-//user logic
+//user logic (Front-end)
