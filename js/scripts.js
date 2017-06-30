019b8673b3bb270c935987c9b7dc60381d624315
Michael Brooks <mikeyabrooks@gmail.com>
2017-06-29 20:57:57 -0700

    ad an array loop to clean up code and fix earlier syntax.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
+var answer = [];
+
 //business logic (Back-end)
 
 var conditional = function (userInput) {
   for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
     if (currentNumber % 15 === 0) {
-      $("#unorderedList").append("<li>PING PONG!</li>");
+      answer.push("PingponG");
     } else if (currentNumber % 5 === 0) {
-      $("#unorderedList").append("<li>pong</li>");
+      answer.push("pong");
     } else if (currentNumber % 3 === 0) {
-      $("#unorderedList").append("<li>ping</li>");
-    } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
-      $("#unorderedList").append("<li>" + currentNumber + "</li>");
+      answer.push("ping");
+    } else {
+      answer.push(currentNumber);
     }
   };
+  return answer;
 };
 
 //user logic (Front-end)
 
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    event.preventDefault();
     $("#unorderedList").empty();
     var userInput = parseInt($("input#inputNumber").val());
+    var results = conditional(userInput);
     if (isNaN(userInput)) {
       alert("You must enter a number!");
     } else if (userInput <= 0) {
       alert("Enter a number higher than 0!");
     } else {
+      for (var currentNumber = 0; currentNumber < userInput; currentNumber += 1) {
+        $("#unorderedList").append("<li>" + results[currentNumber] + "</li>");
+      }
       $("#results").show();
     };
-    conditional(userInput);
-    event.preventDefault();
   });
 });
